b0f27a0d88ee75bd8b10d733a84a85c2e5a58040
Jschles1 <jschles1@ramapo.edu>
2017-12-29 00:21:32 -0500

    Created rootReducer, imported react-bootstrap into index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 import App from './App';
+import 'react-bootstrap';
 import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(
