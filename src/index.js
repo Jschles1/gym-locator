d6d65100f42972bd14de3dd2a06c58c1037324cc
Jschles1 <jschles1@ramapo.edu>
2017-12-29 21:14:37 -0500

    Deleted boilerplate code in App.css, imported index.css into index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 import App from './App';
 import 'react-bootstrap';
+import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(
