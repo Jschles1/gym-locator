8b7df21184b82f24a650ea6148456eb1534acec2
Jschles1 <jschles1@ramapo.edu>
2017-12-29 22:15:58 -0500

    Changed Navbar to use bootstrap Navbar, added link elements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import { PageHeader } from 'react-bootstrap';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import './Navbar.css';
 
-const Navbar = () => 
-  <PageHeader>
-    Gym Locator
-  </PageHeader>
+const NavbarComponent = () => 
+  <Navbar>
+    <Navbar.Header className="main-header">
+      <h1>Gym Locator</h1>
+      <Nav className="links">
+        <NavItem href="#">Search</NavItem>
+        <NavItem href="#">Starred Gyms</NavItem>
+        <NavItem href="#">About Me</NavItem>
+      </Nav>
+    </Navbar.Header>
+    
+  </Navbar>
 
-export default Navbar;
+export default NavbarComponent;
