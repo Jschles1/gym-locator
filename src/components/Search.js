680d3e3d92b69f14df16ca42e2ab3e833f43c0ed
Jschles1 <jschles1@ramapo.edu>
2017-12-31 21:39:10 -0500

    Created Search component and imported into App

---
+++ src/components/Search.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <p>Search Form</p>
+    );
+  }
+}
+
+export default Search;
