047f11ccea9f93f1bc0f478e1318acfa3ff483dd
Jschles1 <jschles1@ramapo.edu>
2018-01-01 23:24:04 -0500

    Reformatted Navbar & edited styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,15 +4,16 @@ import './Navbar.css';
 
 const NavbarComponent = () => 
   <Navbar>
-    <Navbar.Header className="main-header">
-      <h1>Gym Locator</h1>
-      <Nav className="links">
-        <NavItem href="#">Search</NavItem>
-        <NavItem href="#">Starred Gyms</NavItem>
-        <NavItem href="#">About Me</NavItem>
-      </Nav>
+    <Navbar.Header>
+      <Navbar.Brand className="main-header">
+        <h1>Gym Locator</h1>
+      </Navbar.Brand>
     </Navbar.Header>
-    
+    <Nav pullRight className="links">
+      <NavItem href="#">Search</NavItem>
+      <NavItem href="#">Starred Gyms</NavItem>
+      <NavItem href="#">About Me</NavItem>
+    </Nav>
   </Navbar>
 
 export default NavbarComponent;
