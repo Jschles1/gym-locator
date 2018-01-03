5a2fb8d3d24670d61ae2feda381d71e90aed4a3c
Jschles1 <jschles1@ramapo.edu>
2018-01-02 22:21:20 -0500

    Defined initial state, imported actions, connect & bindActionCreators, connected component to store, added Radius input

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions/searchActions';
+import { bindActionCreators } from 'redux';
 import { Form, FormControl, ControlLabel, Button, FormGroup } from 'react-bootstrap';
 import './Search.css';
 
@@ -6,7 +9,11 @@ class Search extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      gymType: '',
+      zipCode: 0,
+      radius: 25
+    };
   }
 
   render() {
@@ -24,6 +31,12 @@ class Search extends Component {
           <FormControl type="number" pattern="[0-9]{5}" />
         </FormGroup>
           {' '}
+        <FormGroup controlId="formInlineRadius">
+          <ControlLabel>Radius</ControlLabel>
+          {' '}
+          <FormControl type="number" pattern="[0-9]{5}" />
+        </FormGroup>
+          {' '}
         <Button type="submit">
           Search
         </Button>
@@ -32,4 +45,12 @@ class Search extends Component {
   }
 }
 
-export default Search;
+const mapStateToProps = (state) => {
+  return { errors: state.errors };
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return { actions: bindActionCreators(actions, dispatch) }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Search);
