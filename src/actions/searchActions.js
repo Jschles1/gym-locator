1ad4a6c22cabc3f60b3dd06cff07e3c087b733ab
Jschles1 <jschles1@ramapo.edu>
2018-01-02 23:18:56 -0500

    Built out searchGyms (Must now build backend since Yelp doesn't support CORS)

--- src/actions/searchActions.js
+++ src/actions/searchActions.js
@@ -3,7 +3,23 @@ import axios from 'axios';
 export function searchGyms(query) {
   return function(dispatch) {
     dispatch({ type: 'LOADING' });
-    return axios.get('https://api.yelp.com/v3/businesses/search')
-      .then(gyms => dispatch({ type: 'GET_RESULTS', payload: gyms }));
+    return axios({
+      method: 'get',
+      url: 'https://api.yelp.com/v3/businesses/search',
+      headers: {
+        'Authorization': `Bearer ${process.env.REACT_API_KEY}`
+      },
+      params: {
+        term: 'gym',
+        location: '07676',
+        categories: 'fitness',
+        limit: '25'
+      }
+    })
+      .then(response => console.log(response))
+      // .then(gyms => dispatch({ type: 'GET_RESULTS', payload: gyms }))
+      .catch(error => {
+        console.log(error)
+      })
   }
 }
