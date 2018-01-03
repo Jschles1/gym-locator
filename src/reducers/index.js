5034d39ed0b74b885f730ef5cd03b340dcad7450
Jschles1 <jschles1@ramapo.edu>
2018-01-02 21:58:15 -0500

    Created resultsReducer & imported into rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import loadingReducer from './loadingReducer';
+import resultsReducer from './resultsReducer';
 
 const rootReducer = combineReducers({
-  loading: loadingReducer
+  loading: loadingReducer,
+  results: resultsReducer
 });
 
 export default rootReducer;
